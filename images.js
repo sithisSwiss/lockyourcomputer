0354b9e9c47326166503fb8467ca06094b9b2ea8
Roman Spring <roman.spring@oetiker.com>
2025-12-03 12:09:49 +0100

    feat: Add Subpage for all images and split css and urls

---
+++ images.js
@@ -0,0 +1,10 @@
+const imageUrls = [
+    "https://media.makeameme.org/created/lock-your-computer-94f4d141f2.jpg",
+    "https://i.imgur.com/RIN87.jpg",
+    "https://media.makeameme.org/created/for-the-last-d5032aa529.jpg",
+    "https://us1.discourse-cdn.com/spiceworks/original/4X/5/6/e/56e2f06bb2ff7efaff13d8eb1287fba24efbbdfb.jpeg",
+    "https://i.imgflip.com/add265.jpg",
+    "https://i.imgur.com/RIN87.jpg",
+    "https://media.makeameme.org/created/if-you-could-70da9d20d5.jpg",
+    "https://media.makeameme.org/created/lock-your-computer-dd47f63301.jpg"
+];
